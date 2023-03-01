27bfae75f35b19aa5b7f8b56da8d97fc334e41de
PimployPungnin <tangmo00pp@gmail.com>
2023-03-02 00:01:56 +0700

    Update webboard_recipes.sql

--- webboard_recipes.sql
+++ webboard_recipes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2023 at 03:25 PM
+-- Generation Time: Mar 01, 2023 at 05:47 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -29,8 +29,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `category` (
-  `id` int(100) NOT NULL,
-  `type` varchar(64) NOT NULL
+  `id_category` int(100) NOT NULL,
+  `tag` varchar(64) NOT NULL,
+  `food_country` varchar(100) NOT NULL,
+  `food_type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -41,7 +43,10 @@ CREATE TABLE `category` (
 
 CREATE TABLE `comment` (
   `user_id` int(11) NOT NULL,
-  `user_name` varchar(100) NOT NULL
+  `user_name` varchar(100) NOT NULL,
+  `comment_id` int(100) NOT NULL,
+  `post_id` int(100) NOT NULL,
+  `c_content` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -51,11 +56,17 @@ CREATE TABLE `comment` (
 --
 
 CREATE TABLE `post` (
-  `id` int(100) NOT NULL,
-  `user_name` varchar(100) NOT NULL,
+  `id_post` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
   `title` varchar(1000) NOT NULL,
+  `m_content` varchar(2024) NOT NULL,
   `content` varchar(2048) NOT NULL,
-  `post_date` datetime NOT NULL
+  `picture` varchar(500) NOT NULL,
+  `post_date` datetime NOT NULL,
+  `post_like` int(100) NOT NULL,
+  `post_dislike` int(100) NOT NULL,
+  `post_view` int(100) NOT NULL,
+  `id_category` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -67,8 +78,9 @@ CREATE TABLE `post` (
 CREATE TABLE `user` (
   `user_id` int(100) NOT NULL,
   `user_name` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `email` varchar(64) NOT NULL
+  `username` varchar(64) NOT NULL,
+  `user_password` varchar(64) NOT NULL,
+  `user_email` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -79,19 +91,22 @@ CREATE TABLE `user` (
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_category`);
 
 --
 -- Indexes for table `comment`
 --
 ALTER TABLE `comment`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `post_id` (`post_id`);
 
 --
 -- Indexes for table `post`
 --
 ALTER TABLE `post`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_post`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `id_category` (`id_category`);
 
 --
 -- Indexes for table `user`
@@ -107,13 +122,30 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_post` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id_post`);
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `post_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
