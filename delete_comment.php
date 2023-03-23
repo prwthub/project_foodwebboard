909538dcf6e5f5f34a2ee3b59d4894dab2c92b18
prwt <perawit37@gmail.com>
2023-03-23 14:11:08 +0700

    fix delete_comment

--- delete_comment.php
+++ delete_comment.php
@@ -6,13 +6,18 @@
     $password = "";
     $database = "webboard_recipes";
 
+    $id = $_GET["id"];
     $uid = $_SESSION['user_id'];
     $cid = $_SESSION['comment_id'];
     $pid = $_SESSION['post_id'];
 
+    // echo "id = ".$id;
+    // echo "<BR>user_id = ".$uid;
+    // echo "<BR>comment_id = ".$cid;
+    // echo "<BR>post_id = ".$pid;
 
     $conn = new PDO("mysql:host=$server_name;dbname=$database;charset=utf8", "$username", "$password");
-    $sql_comment = "DELETE FROM comment WHERE post_id = $pid and comment_id = $cid";
+    $sql_comment = "DELETE FROM comment WHERE post_id = $pid and comment_id = $id";
     $conn->query($sql_comment);
 
     unset($_SESSION['comment_id']);
