a7057d60b2649ea7b0ee8aed0105215c97ec750c
ThanachartPra <110073990+ThanachartPra@users.noreply.github.com>
2023-04-09 21:37:33 +0700

    UX/UI Register

--- register.php
+++ register.php
@@ -9,9 +9,12 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
     <!--Icon-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
@@ -35,6 +38,21 @@ session_start();
             }
         }
     </script>
+    <style>
+        .responsiveImage {
+            width: 100%;
+            height: 35vw;
+            object-fit: cover;
+
+        }
+
+        .responsiveImagelogo {
+            width: 140pt;
+            height: 80%;
+            object-fit: cover;
+
+        }
+    </style>
 </head>
 
 <body>
@@ -45,95 +63,126 @@ session_start();
     ?>
     <?php
     include "nav.php";
-    echo "<BR>"
     ?><!-- เรียก navbar จากที่อื่นมาใช้ ง่ายต่อการเรียนใช้ในหลายๆไฟล์ -->
-    <div class="container">
-        <form action="register_save.php" method="post">
 
-            <!--------------------------------------------------------------------------------------------->
-            <!-- session ที่เอาไว้เตือนว่า register สำเร็จหรือไม่สำเร็จ -->
-            <?php
-            if (isset($_SESSION["add_login"])) {
+    <section class="vh-100 pb-1" style="background-color: #9A616D; ">
+        <br>
+        <?php
+        if (isset($_SESSION["add_login"])) {
             ?>
-                <div class="row mt-3">
-                    <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        <?php
-                        // register error
-                        if ($_SESSION["add_login"] == 'error') {
-                            echo "<div class=\"alert alert-danger bi bi-x-circle\" role=\"alert\">";
-                            echo "  ชื่อบัญชีซ้ำหรืออีเมลล์ถูกใช้ไปแล้ว";
-                            echo "</div>";
-                            unset($_SESSION["add_login"]);
-                        }
-                        // register success
-                        else if ($_SESSION["add_login"] == 'success') {
-                            echo "<div class=\"alert alert-success bi bi-check-circle\" role=\"alert\">";
-                            echo "  เพิ่มบัญชีเรียบร้อยแล้ว";
-                            echo "</div>";
-                            unset($_SESSION["add_login"]);
-                        }
-                        ?>
-                    </div>
-                    <div class="col-md-3"></div>
-                </div>
-            <?php
-            }
-            ?>
-            <!--------------------------------------------------------------------------------------------->
-            <div class="row">
-                <div class="col-md-3"> </div>
-
+            <div class="row mt-3">
+                <div class="col-md-3"></div>
                 <div class="col-md-6">
-                    <div class="card text-dark bg-white border-primary">
-                        <div class="card-header bg-primary text-white ">กรอกข้อมูล</div>
-                        <div class="card-body">
-                            <div class="row mb-3 ">
-                                <label class="col-md-3 col-form-label">username :</label>
-                                <div class="col-md-9">
-                                    <input type="text" class="form-control" name="login" required>
+                    <?php
+                    // register error
+                    if ($_SESSION["add_login"] == 'error') {
+                        echo "<div class=\"alert alert-danger bi bi-x-circle\" role=\"alert\">";
+                        echo "  ชื่อบัญชีซ้ำหรืออีเมลล์ถูกใช้ไปแล้ว";
+                        echo "</div>";
+                        unset($_SESSION["add_login"]);
+                    }
+                    // register success
+                    else if ($_SESSION["add_login"] == 'success') {
+                        echo "<div class=\"alert alert-success bi bi-check-circle\" role=\"alert\">";
+                        echo "  เพิ่มบัญชีเรียบร้อยแล้ว";
+                        echo "</div>";
+                        unset($_SESSION["add_login"]);
+                    }
+                    ?>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+            <?php
+        }
+        ?>
+        <?php
+        // ถ้ามี session มาจะมีการแสดงผล
+        if (isset($_SESSION["error"])) {
+            echo "<div class='alert alert-danger'><i class='bi bi-exclamation-circle-fill'></i>  ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง</div>";
+            // unset ไว้ เมื่อแจ้งเตือนเสร็จแล้ว ก็ลบ ไม่งั้นมันจะคาไว้
+            unset($_SESSION["error"]);
+        }
+        ?>
+
+
+        <div class="container py-1 h-90">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-xl-10">
+                    <div class="card" style="border-radius: 1rem;">
+                        <div class="row g-0">
+                            <?php
+                            $min = 1;
+                            $max = 3;
+                            $randomNumber = rand($min, $max);
+                            //echo $randomNumber;
+                            ?>
+
+                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+
+                                <div class="card-body p-1 p-lg-4 text-black">
+
+                                    <form action="register_save.php" method="post">
+
+                                        <div class="d-flex justify-content-center align-items-center mb-1 pb-1">
+                                            <i class="fas fa-cubes fa-2x me-1" style="color: #ff6219;"></i>
+                                            <img src="food/logo.png" alt="login form"
+                                                class="img-fluid responsiveImagelogo"
+                                                style="border-radius: 1rem 0 0 1rem;" />
+                                        </div>
+
+
+
+                                        <div class="form-outline mb-3">
+                                            <h6 class="fw-normal mb-2 pb-1"
+                                                style="letter-spacing: 1px; color: #393f81;">Register</h6>
+                                            <input type="text" name="login" class="form-control"
+                                                class="form-control form-control-lg" required
+                                                placeholder='username or email'>
+                                        </div>
+
+                                        <div class="input-group form-outline mb-3">
+                                            <input type="password" name="pwd" class="form-control" id="pwd"
+                                                class="form-control form-control-lg" required placeholder='password'>
+                                            <span class="input-group-text" onclick="password_show_hide();">
+                                                <i class="bi bi-eye-fill" id="show_eye"></i>
+                                                <i class="bi bi-eye-slash-fill d-none" id="hide_eye"></i>
+                                            </span>
+
+                                        </div>
+
+                                        <div class="form-outline mb-3">
+                                            <input type="text" name="name" class="form-control"
+                                                class="form-control form-control-lg" required
+                                                placeholder='username account'>
+                                        </div>
+
+                                        <div class="form-outline mb-3">
+                                            <input type="text" name="email" class="form-control"
+                                                class="form-control form-control-lg" required placeholder='email'>
+                                        </div>
+
+                                        <div class="pt-1 mb-4">
+                                            <button class="btn btn-dark btn-lg btn-block"
+                                                type="submit">Register</button>
+                                        </div>
+
+                                        <a href="#!" class="small text-muted">Terms of use.</a>
+                                        <a href="#!" class="small text-muted">Privacy policy</a>
+                                    </form>
+
                                 </div>
                             </div>
-                            <div class="row mb-3 ">
-                                <label class="col-md-3 col-form-label">password :</label>
-                                <div class="col-md-9">
-                                    <div class="input-group">
-                                        <input type="password" class="form-control" name="pwd" required id="password">
-                                        <span class="input-group-text " onclick="password_show_hide();">
-                                            <i class="bi bi-eye-fill" id="show_eye"></i>
-                                            <i class="bi bi-eye-slash-fill d-none" id="hide_eye"></i>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label class="col-md-3 col-form-label">account name :</label>
-                                <div class="col-md-9">
-                                    <input type="text" class="form-control" name="name" require>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label class="col-md-3 col-form-label">email :</label>
-                                <div class="col-md-9">
-                                    <input type="email" class="form-control" name="email" required>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label class="col-md-3 col-form-label"></label>
-                                <div class="col-md-9">
-                                    <button type="submit" class="btn btn-primary btn-sm bi bi-save"> สมัครสมาชิก </button>
-                                </div>
+                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <?php
+                                echo "<img src='food/$randomNumber.png' alt='login form' class='img-fluid responsiveImage' style='border-radius: 0 1rem 1rem 0;' />"
+                                    ?>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div class="col-md-3"> </div>
-    </div>
-    </form>
-
-    </div>
-</body>
+    </section>
 
 </html>
